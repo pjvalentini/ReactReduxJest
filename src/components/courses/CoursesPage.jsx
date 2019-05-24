1ae854366ed7cf0201d5ffccd7c170f3f80d3d46
pj valentini <pj.valentini@gmail.com>
2019-05-24 16:15:50 -0500

    Made changes to babel config and package.json (babel)

--- src/components/courses/CoursesPage.jsx
+++ src/components/courses/CoursesPage.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
-  constructor(props) {
-    super(props);
+  // we dont need the constructor here or the super(props) as we are using a class property set up here. New Stuff!
+  state = {
+    course: {
+      title: ""
+    }
+  };
 
-    this.state = {
-      course: {
-        title: ""
-      }
-    };
-  }
+  // this a class field. We can safely us this but will be standard in +/-2020.
+  handleChange = e => {
+    // the spread operator will copy th course from state, and overwrite the title.
+    // with the spread op - the values on the right override those on the left.
+    const course = { ...this.state.course, title: e.target.value };
+
+    // this.setState({ course: course }); we can use obj shorthand here to shorten the code.
+    this.setState({ course });
+  };
 
   render() {
     return (
       <form action="">
         <h2>Courses</h2>
-        <h3>Add Course</h3>
+        <h4>Add Course</h4>
         <input
           type="text"
           onChange={this.handleChange}
