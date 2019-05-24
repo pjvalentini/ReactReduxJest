2d3e0dc8dc743da0051f0f9a3e5f8faaad1e660e
pj valentini <pj.valentini@gmail.com>
2019-05-24 08:58:46 -0500

    added a Header component to the newly added commom folder in src

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./home/HomePage.jsx";
 import AboutPage from "./about/AboutPage.jsx";
+import Header from "./common/Header.jsx";
 
 function App() {
     return (
         <div className="containewr-fluid">
+            <Header />
             <Route exact path="/" component={HomePage} />
             <Route path="/about" component={AboutPage} />
         </div>
