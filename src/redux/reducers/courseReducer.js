956bb88442771998634f67624f860422791bfdf4
pj valentini <pj.valentini@gmail.com>
2019-05-29 10:33:43 -0500

    added reducer for the new actions

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -4,10 +4,14 @@ import initialState from "./initialState";
 export default function courseReducer(state = initialState.courses, action) {
   // Here we create a swtich that looks at the action type
   switch (action.type) {
-    case types.CREATE_COURSE:
+    case types.CREATE_COURSE_SUCCESS:
       // we return a new copy of state and the passed in course
       // what ever is returned from the reducer becomes the new state for that particular reducer.
       return [...state, { ...action.course }];
+    case types.UPDATE_COURSE_SUCCESS:
+      return state.map(course =>
+        course.id === action.course.id ? action.course : course
+      );
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
     default:
