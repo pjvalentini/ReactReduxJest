05f42aa9cb882b475b428f698d187ec45dafa1bb
pj valentini <pj.valentini@gmail.com>
2019-05-29 16:00:19 -0500

    Added action for when api calls are made. This will keep track of API calls in progress

---
+++ src/redux/reducers/apiStatusReducer.js
@@ -0,0 +1,21 @@
+import * as types from "../actions/actionTypes";
+import initialState from "./initialState";
+
+// helper function to detmine if the api call will end in success usign substring
+function actionTypeEndsInSuccess(type) {
+  return type.substring(type.length - 8) === "_SUCCESS";
+}
+
+// Will determine the number of apiCalls in progress.
+// This will handle the same action type in multiple reducers.
+export default function apiCallStatusReducer(
+  state = initialState.apiCallsInProgress,
+  action
+) {
+  if (action.type == types.BEGIN_API_CALL) {
+    return state + 1;
+  } else if (actionTypeEndsInSuccess(action.type)) {
+    return state - 1;
+  }
+  return state;
+}
--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -1,5 +1,6 @@
 // this shows the shape of the store.  Initially set to empty arrays.
 export default {
   courses: [],
-  authors: []
+  authors: [],
+  apiCallsInProgress: 0
 };
