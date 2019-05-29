eb55293399a64ce0baa42c41857bbb03919dbffd
pj valentini <pj.valentini@gmail.com>
2019-05-29 10:25:16 -0500

    added createCourseSuccess and updateCourseSucess Actions and types. Added saveCouse Thunk

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -3,12 +3,17 @@ import * as courseApi from "../../api/courseApi";
 
 // Naming conv: verbNoun
 // Requires a type and usually a payload
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, course };
-}
 
 export function loadCourseSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
 }
 
 export function loadCourses() {
@@ -17,6 +22,21 @@ export function loadCourses() {
       .getCourses()
       .then(courses => {
         dispatch(loadCourseSuccess(courses));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return courseApi
+      .saveCourse(course)
+      .then(savedCourse => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
       })
       .catch(error => {
         throw error;
