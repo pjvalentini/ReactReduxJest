7d3830a96a2ca434588a2bc38bc43cf224b80541
pj valentini <pj.valentini@gmail.com>
2019-05-31 11:18:39 -0500

    added a thunk test to test if certaijn actions were being called when the API call is made

--- src/redux/actions/CourseActions.test.js
+++ src/redux/actions/CourseActions.test.js
@@ -1,6 +1,41 @@
 import * as courseActions from "./courseActions";
 import * as types from "./actionTypes";
 import { courses } from "../../../tools/mockData";
+// for testing thunks we need some additional imports
+import thunk from "redux-thunk";
+import fetchMock from "fetch-mock"; // for mocking fetch calls
+import configureMockStore from "redux-mock-store"; // for creating a mock redux store
+
+// test an async action
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe("Async Actions", () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  describe("Load Courses Thunk", () => {
+    it("should create BEGIN_API_CALL and LOAD_COURSES_SUCCESS when loading courses", () => {
+      // captures all fetch calls and the response is mock data.
+      fetchMock.mock("*", {
+        body: courses,
+        headers: { "content-type": "application/json" }
+      });
+
+      // assert that both of loadCourses() thunk will call these two actions when loading courses
+      const expectedActions = [
+        { type: types.BEGIN_API_CALL },
+        { type: types.LOAD_COURSES_SUCCESS, courses }
+      ];
+
+      const store = mockStore({ courses: [] });
+      return store.dispatch(courseActions.loadCourses()).then(() => {
+        expect(store.getActions()).toEqual(expectedActions);
+      });
+    });
+  });
+});
 
 // using arrange/assert/act pattern
 // this test confirms that when I call the createCourseSuccess action creator, I get the expected onject shape back.
